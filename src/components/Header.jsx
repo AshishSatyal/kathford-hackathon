8f812b0d59ecaf6820b3f7a0945fbaae80f0b07e
AshishSatyal <ashishsatyal4@gmail.com>
2024-05-20 22:57:08 +0545

    logo added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import kathCode from "/src/assets/kathCode.png";
 
 const Header = () => {
   return (
-    <div className='fixed top-0 z-50 bg-color-1 backdrop-blur-sm border-b lg:backdrop-blur-sm'>
-      <div></div>
+    <div className='fixed top-0'>
+      <div className='flex items-center px-5 lg:px-7 xl:px-10 max-lg:py-4'>
+        <a href='#hero' className='block w-[12rem] xl:mr-8 '>
+          <img src={kathCode} alt='logo' width={200} height={40} />
+        </a>
+      </div>
     </div>
   );
 };
