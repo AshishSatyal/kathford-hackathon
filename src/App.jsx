f32166b50b543004fec2c7ccf9ce172b9d7fd671
Kanak-dev <kanak24.fv@gmail.com>
2024-05-20 22:59:53 +0545

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import Header from "./components/Header";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const App = () => {
+function App() {
   return (
     <>
-      <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
-        <Header />
-      </div>
+      <h1>APpp</h1>
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
