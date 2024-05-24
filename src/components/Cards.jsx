3423a23493396ba9a3e0223b1a24c47cbaba8dd9
AshishSatyal <ashishsatyal4@gmail.com>
2024-05-24 10:51:41 +0545

    bg changed

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Cards = ({ position, prize, other, img, bg }) => {
   return (
-    <div className={`p-5 h-52 rounded-xl ${bg}`}>
+    <div className={`p-5 sm:h-72 lg:h-52 rounded-xl ${bg} w-[50%] lg:w-[30%]`}>
       <div className='flex flex-col items-start px-5'>
         <div className='flex items-center justify-between'>
           <p className='font-bold text-4xl font-mono text-slate-200 capitalize'>
--- src/components/Prizes.jsx
+++ src/components/Prizes.jsx
@@ -6,14 +6,14 @@ import Cards from "./Cards";
 
 const Prizes = () => {
   return (
-    <div className='bg-color-4 min-h-[85vh] py-5 lg:px-10'>
+    <div className='bg-color-2 min-h-[85vh] py-5 lg:px-10'>
       <div className='my-5 text-center '>
         <p className='text-white text-3xl lg:text-5xl capitalize font-semibold font-[robotomono]'>
           prizes
         </p>
       </div>
       <div className='p-5 lg:min-w-full'>
-        <p className='text-center text-slate-300 font-normal text-xl lg:text-2xl capitalize font-mono tracking-tight mt-5'>
+        <p className='text-center text-slate-300 font-normal text-xl lg:text-2xl capitalize font-mono tracking-tight'>
           win from the prize pool of{" "}
           <span className='text-color-1 font-bold text-2xl'>Rs.50000</span>
         </p>
@@ -27,7 +27,7 @@ const Prizes = () => {
           img={winner}
         />
         <Cards
-          bg={"bg-gradient-to-r from-color-2/50 to-color-3/70"}
+          bg={"bg-gradient-to-r from-color-1/50 to-color-3/70"}
           position={"1st runner up"}
           prize={"Rs.15000"}
           other={"medals & Certificate"}
