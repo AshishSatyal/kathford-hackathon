f03c6f5274459b122224478d0235fc8d3df33da3
AshishSatyal <ashishsatyal4@gmail.com>
2024-05-28 16:59:08 +0545

    bug fixed: countdown timer leading zero

--- src/components/Dayplan.jsx
+++ src/components/Dayplan.jsx
@@ -5,7 +5,7 @@ import { variantNav } from "../utils/variants";
 const Dayplan = ({ day }) => {
   return (
     <motion.div
-      className='bg-color-3/90 lg:w-[40%] sm:w-[70%] rounded-xl p-5'
+      className='bg-color-1/70 lg:w-[40%] sm:w-[70%] rounded-xl p-5'
       initial='offscreen'
       whileInView={"onscreen"}
       viewport={{
@@ -13,28 +13,28 @@ const Dayplan = ({ day }) => {
       }}
       variants={variantNav}
     >
-      <p className='text-white text-center capitalize text-3xl font-[robotomono] pb-4 border-b'>
+      <p className='text-white text-center capitalize text-3xl font-[robotomono] py-4 border-b[x]'>
         {day}
       </p>
-      <div className='flex flex-col items-start justify-evenly mt-5 p-2 text-white'>
+      <div className='flex flex-col items-start justify-evenly mt-5[x] p-2 text-white'>
         <p className=' sm:text-md lg:text-xl font-mono capitalize p-2'>
-          <span className='text-slate-200'>7:00 AM</span>: Check in and
+          <span className='text-slate-200'>07:00 AM</span>: Check in and
           registration
         </p>
         <p className=' sm:text-md lg:text-xl font-mono capitalize p-2'>
-          <span className='text-slate-200'>8:00 AM</span>: Inaguration
+          <span className='text-slate-200'>08:00 AM</span>: Inaguration
         </p>
         <p className=' sm:text-md lg:text-xl font-mono capitalize p-2'>
-          <span className='text-slate-200'>9:00 AM</span>: Lunch
+          <span className='text-slate-200'>09:00 AM</span>: Lunch
         </p>
         <p className=' sm:text-md lg:text-xl font-mono capitalize p-2'>
           <span className='text-slate-200'>10:00 AM</span>: Hackathon starts!
         </p>
         <p className=' sm:text-md lg:text-xl font-mono capitalize p-2'>
-          <span className='text-slate-200'>2:30 PM</span>: snacks break
+          <span className='text-slate-200'>02:30 PM</span>: snacks break
         </p>
         <p className=' sm:text-md lg:text-xl font-mono capitalize p-2'>
-          <span className='text-slate-200'>7:30 PM</span>: Dinner break
+          <span className='text-slate-200'>07:30 PM</span>: Dinner break
         </p>
       </div>
     </motion.div>
