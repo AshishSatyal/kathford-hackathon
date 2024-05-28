9dc7a51f20e53ee05897a1d0d8dfd8481887fa18
Kanak-dev <kanak24.fv@gmail.com>
2024-05-28 12:03:20 +0545

    feat: add link to the honeycomb cell in the partners

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -2,12 +2,18 @@ import React from 'react'
 
 const Cell = ({ src, alt_text }) => {
   return (
-    // <a href="#">
     <li className="honeycomb-cell">
+      <a 
+        href="https://khalti.com/" 
+        target='_blank'
+        className='block w-full h-[200%] mt-[-50px]'>
+
+        {/* if silver the box-shadow will be silver */}
+        
         <img src={src} alt={alt_text} className="honeycomb-cell_img " width={100} height={100}/>
-        <div className="honeycomb-cell_title">Khalti</div>
+      </a>
+        {/* <div className="honeycomb-cell_title">Khalti</div> */}
     </li>
-    // </a>
   )
 }
 
