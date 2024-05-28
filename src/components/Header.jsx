ac5c21580237dcf61af14498f82c8c21de35160a
Bishal Pahari <bishal.pahari53@gmail.com>
2024-05-28 23:19:18 +0545

    feat:remove background

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,32 @@
-import React from "react";
-import kathCode from "/src/assets/kathCode.png";
-import hamBurger from "/src/assets/hamBurger.svg";
-import Navlinks from "./Navlinks";
+import React from 'react';
+import kathCode from '/src/assets/kathCode.png';
+import hamBurger from '/src/assets/hamBurger.svg';
+import Navlinks from './Navlinks';
 
 const Header = () => {
   return (
     // --Header-container--
-    <div className='sticky top-0 px-[2.5rem] py-4 bg-color-5 min-w-full text-white z-10'>
-      <div className='flex items-center xl:px-10'>
+    <div className="sticky top-0 px-[2.5rem] py-4  min-w-full text-white z-10">
+      <div className="flex items-center xl:px-10">
         <div>
-          <a href='#hero' className='block w-[12rem] xl:mr-8 '>
+          <a href="#hero" className="block w-[12rem] xl:mr-8 ">
             <img
-              className=' w-[100px] h-[40px] sm:w-[auto] sm:h-[auto]'
+              className=" w-[100px] h-[40px] sm:w-[auto] sm:h-[auto]"
               src={kathCode}
-              alt='logo'
+              alt="logo"
               width={200}
               height={40}
             />
           </a>
         </div>
         <Navlinks />
-        <div className='hidden top-[5rem] bottom-0 left-0 right-0 lg:static lg:flex'>
-          <button className='register m-2 p-4 rounded-lg bg-gradient-to-r from-color-1 to-color-3 capitalize  font-mono text-lg font-bold'>
+        <div className="hidden top-[5rem] bottom-0 left-0 right-0 lg:static lg:flex">
+          <button className="register m-2 p-4 rounded-lg bg-gradient-to-r from-color-1 to-color-3 capitalize  font-mono text-lg font-bold">
             register now
           </button>
         </div>
-        <div className='lg:hidden ml-auto'>
-          <img src={hamBurger} alt='HamBurger' width={25} height={25} />
+        <div className="lg:hidden ml-auto">
+          <img src={hamBurger} alt="HamBurger" width={25} height={25} />
         </div>
       </div>
     </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,25 +1,25 @@
-import React from "react";
-import CountDown from "./CountDown";
+import React from 'react';
+import CountDown from './CountDown';
 
 const Hero = () => {
   return (
-    <div className='bg-color-5 min-h-[85vh] px-10 lg:px-[5rem] flex flex-col justify-center items-center'>
-      <div className='w-2/3'>
-        <p className='text-color-1/100 text-center lg:text-6xl text-3xl font-normal font-mono capitalize'>
-          <span className='text-white font-[robotomono] uppercase block pb-2'>
+    <div className=" min-h-[85vh] px-10 lg:px-[5rem] flex flex-col justify-center items-center">
+      <div className="w-2/3">
+        <p className="text-color-1/100 text-center lg:text-6xl text-3xl font-normal font-mono capitalize">
+          <span className="text-white font-[robotomono] uppercase block pb-2">
             unleash your talent
           </span>
           Build the future.
         </p>
-        <p className='text-white/70 text-md font-semibold text-center mt-7 font-mono'>
+        <p className="text-white/70 text-md font-semibold text-center mt-7 font-mono">
           Innovators and problem-solvers,unite! This hackathon <br />
           challenges you to turn your idea into solutions.
         </p>
       </div>
-      <div className='my-4'>
+      <div className="my-4">
         <CountDown />
       </div>
-      <button className=' register m-2 p-4 text-white rounded-lg bg-color-3 capitalize hover:bg-color-2 font-mono text-lg font-bold'>
+      <button className=" register m-2 p-4 text-white rounded-lg bg-color-3 capitalize hover:bg-color-2 font-mono text-lg font-bold">
         Discord
       </button>
     </div>
