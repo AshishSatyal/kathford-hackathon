3e6db11af619b8828e9078f69e31d6fca9a9cb13
Bishal Pahari <bishal.pahari53@gmail.com>
2024-05-28 23:13:15 +0545

    fix: arrange z-index

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,34 @@
-import "./App.css";
-import Header from "./components/Header";
-import Hero from "./components/Hero";
-import Faq from "./components/Faq";
-import Prizes from "./components/Prizes";
-import Tracks from "./components/Tracks";
-import Schedule from "./components/Schedule";
-import Ourteam from "./components/Ourteam";
-import Partners from "./components/Partners";
+import './App.css';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Faq from './components/Faq';
+import Prizes from './components/Prizes';
+import Tracks from './components/Tracks';
+import Schedule from './components/Schedule';
+import Ourteam from './components/Ourteam';
+import Partners from './components/Partners';
+import Blob from './components/Blob';
 
-function App() {
+const App = () => {
   return (
     <>
-      <Header />
-      <Hero />
-      <Prizes />
-      <Tracks />
-      <Schedule />
-      <Ourteam />
-      <Faq />
-      <Partners />
+      <div className="relative">
+        <div className="cursor-pointer absolute z-0">
+          <Blob />
+        </div>
+        <div className="body-section relative z-10">
+          <Header />
+          <Hero />
+          <Prizes />
+          <Tracks />
+          <Schedule />
+          <Ourteam />
+          <Faq />
+          <Partners />
+        </div>
+      </div>
     </>
   );
-}
+};
 
 export default App;
