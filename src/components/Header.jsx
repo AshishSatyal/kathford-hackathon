237090315a379aec8c3afaf53f697cb2989e6aa8
AshishSatyal <ashishsatyal4@gmail.com>
2024-06-02 16:39:44 +0545

    fix bug

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,6 @@ import Button from "./Button";
 
 const Header = () => {
   const [toggle, setToggle] = useState(false);
-  const registration_url = 'https://docs.google.com/forms/d/e/1FAIpQLSduLeNfRW2__b8ClTTUslocDAJ65bkS_oGbyWQ7hdIBMr9e9A/viewform';
   const scrollValue = useScrollPosition();
   const body = document.body;
 
@@ -20,30 +19,36 @@ const Header = () => {
   }
   return (
     // --Header-container--
-    <div className='sticky top-0 px-[2.5rem] py-4 min-w-full text-white z-10 '>
-      <div className='flex items-center xl:px-10'>
-        <div>
-          <a href="#" className="block w-[12rem] xl:mr-8 ">
-            <img
-              className=" w-auto h-[40px] sm:w-[auto] sm:h-[auto]"
-              src={kathCode}
-              alt="logo"
-            />
-          </a>
-        </div>
-        <Navlinks toggleState={toggle} />
+    <>
+      <div className='sticky top-0 px-[2.5rem] py-4 min-w-full text-white z-10 backdrop-blur-xl'>
+        <div className='flex justify-between items-center xl:px-10'>
+          <div>
+            <a href='#hero' className='block w-[12rem] xl:mr-8 '>
+              <img
+                className=' w-[100px] h-[40px] sm:w-[auto] sm:h-[auto]'
+                src={kathCode}
+                alt='logo'
+                width={200}
+                height={40}
+              />
+            </a>
+          </div>
+          <Navlinks scrollState={isHidden} />
 
-
-        <div className='hidden top-[5rem] bottom-0 left-0 right-0 lg:static lg:flex'>
-          <Button label={ "register now" } link={registration_url} />
-        </div>
-        <div className='lg:hidden ml-auto'>
-          <button onClick={toggleNav}>
-            <img src={hamBurger} alt='HamBurger' width={25} height={25} />
-          </button>
+          <div className='hidden top-[5rem] bottom-0 left-0 right-0 lg:static lg:flex items-center'>
+            <Button label={"register now"} />
+            <div className={`${isHidden ? "lg:block" : "lg:hidden"} ml-5`}>
+              <button onClick={toggleNav}>
+                <img src={hamBurger} alt='HamBurger' width={25} height={25} />
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+      <div className={`overlay ${toggle ? "active" : ""} `}>
+        {toggle ? <SideNav setToggle={setToggle} /> : ""}
+      </div>
+    </>
   );
 };
 
