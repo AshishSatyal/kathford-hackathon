eb096382a3353951acc96cc7a1cec81cfd2ebdb6
AshishSatyal <ashishsatyal4@gmail.com>
2024-06-04 10:03:50 +0545

    Add new partner section

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -1,50 +1,40 @@
-import React, { useState, useEffect } from 'react'
-import Cell from './Cell'
-import CenterComponent from './CenterComponent';
+import React, { useState, useEffect } from "react";
+// import Cell from "./Cell";
+import CenterComponent from "./CenterComponent";
+import partners from "../utils/partners";
 
-import khalti from "/src/assets/khalti.jpg";
-import './styles/partners.css';
+// import khalti from "/src/assets/khalti.jpg";
+// import "./styles/partners.css";
 
-import axios from 'axios';
+// import axios from "axios";
 
 const Partners = () => {
-    const [partnersData, setPartnersData] = useState();
+  console.log(partners);
+  const cell = partners.map((item) => {
+    return (
+      <div className='flex flex-col items-center text-white'>
+        <h1 className='font-[monument] text-5xl'>{item.type}</h1>
+        <img src={item.img} width={200} height={100} alt='' />
+        <p>{item.company}</p>
+      </div>
+    );
+  });
 
-    const getPartnersData = async (url) => {
-        const response = await axios.get(url);
-        const data = await response.data;
-        console.log(data.partners)
-        return data.partners;
-    }
-
-    useEffect(() => {
-        const fetchData = async () => {
-            const result = await getPartnersData('/api/partners.json');
-            setPartnersData(result);
-        };
-
-        fetchData();
-    }, []);
-    
-    const cells = partnersData?.map((item, idx) => 
-    <Cell 
-        key={item?.id}
-        src={khalti}
-        alt_text={item?.name}
-    />);
   return (
     <CenterComponent>
-        <section id="partners">
-            <div className="faq-header text-center my-2">
-            <h1 className='text-color-1/100 uppercase text-5xl font-normal'>Partners</h1>
-            </div>
-            <ul className="honeycomb">
-                { cells }
-                <li className="honeycomb-cell honeycomb_hidden"></li>
-            </ul>
-        </section>
+      <section id='partners'>
+        <div className='faq-header text-center my-2'>
+          <h1 className='text-color-1/100 uppercase text-5xl font-normal'>
+            Partners
+          </h1>
+        </div>
+        {/* <ul className='honeycomb'>
+          <li className='honeycomb-cell honeycomb_hidden'></li>
+        </ul> */}
+        {cell}
+      </section>
     </CenterComponent>
-  )
-}
+  );
+};
 
-export default Partners
+export default Partners;
