fe3e3451ebd7a6af750c6381e5244fbfe0697120
AshishSatyal <ashishsatyal4@gmail.com>
2024-06-06 18:11:00 +0545

    fix: img changes in our team

--- src/utils/team.js
+++ src/utils/team.js
@@ -1,83 +1,83 @@
 // import human from "../assets/humanoid1.jpg";
-// import aayush from "../assets/club_member/aayush.jpg";
-// import Abiral from "../assets/club_member/Abiral.jpg";
-// import bishal from "../assets/club_member/bishal.JPG";
-// import Deepti from "../assets/club_member/Deepti.jpg";
-// import dikshita from "../assets/club_member/dikshita.JPG";
+import aayush from "../assets/club_member/aayush.png";
+import Abiral from "../assets/club_member/Abiral.png";
+import bishal from "../assets/club_member/bishal.png";
+import Deepti from "../assets/club_member/Deepti.png";
+import dikshita from "../assets/club_member/dikshita.png";
 import Maharshi from "../assets/club_member/Maharshi.PNG";
-// import nimesh from "../assets/club_member/nimesh.jpg";
-// import rahul from "../assets/club_member/rahul.JPG";
-// import Raisha from "../assets/club_member/Raisha.JPG";
-// import ritika from "../assets/club_member/ritika.JPG";
-// import sebika from "../assets/club_member/sebika.JPG";
+import nimesh from "../assets/club_member/nimesh.png";
+import rahul from "../assets/club_member/rahul.png";
+import Raisha from "../assets/club_member/Raisha.png";
+import ritika from "../assets/club_member/ritika.png";
+import sebika from "../assets/club_member/sebika.png";
 
 const users = [
   {
     id: 1,
     name: "Abiral Blon",
     position: "President",
+    image: Abiral,
+  },
+  {
+    id: 2,
+    name: "Sebika Nepal",
+    position: "Vice-president",
+    image: sebika,
+  },
+  {
+    id: 3,
+    name: "Bishal Pahari",
+    position: "Tech coordinator",
+    image: bishal,
+  },
+  {
+    id: 4,
+    name: "Deepti Basnet",
+    position: "Secretary",
+    image: Deepti,
+  },
+  {
+    id: 5,
+    name: "Aayush Timalsina",
+    position: "Treasurer",
+    image: aayush,
+  },
+  {
+    id: 6,
+    name: "Nimesh Pokhrel",
+    position: "Public Relations",
+    image: nimesh,
+  },
+  {
+    id: 7,
+    name: "Rahul Yadav",
+    position: "Event Coordinator",
+    image: rahul,
+  },
+  {
+    id: 8,
+    name: "Raisha Pradhananga",
+    position: "Executive Member",
+    image: Raisha,
+  },
+  {
+    id: 9,
+    name: "Dikshita Subedi",
+    position: "Executive Member",
+    image: dikshita,
+  },
+  {
+    id: 10,
+    name: "Ritika Suwal",
+    position: "Executive Member",
+    image: ritika,
+  },
+  {
+    id: 11,
+    name: "Maharshi Kattel",
+    position: "Executive Member",
     image: Maharshi,
   },
-  // {
-  //   id: 2,
-  //   name: "Sebika Nepal",
-  //   position: "Vice-president",
-  //   image: sebika,
-  // },
-  // {
-  //   id: 3,
-  //   name: "Bishal Pahari",
-  //   position: "Tech coordinator",
-  //   image: bishal,
-  // },
-  // {
-  //   id: 4,
-  //   name: "Deepti Basnet",
-  //   position: "Secretary",
-  //   image: Deepti,
-  // },
-  // {
-  //   id: 5,
-  //   name: "Aayush Timalsina",
-  //   position: "Treasurer",
-  //   image: aayush,
-  // },
-  // {
-  //   id: 6,
-  //   name: "Nimesh Pokhrel",
-  //   position: "Public Relations",
-  //   image: nimesh,
-  // },
-  // {
-  //   id: 7,
-  //   name: "Rahul Yadav",
-  //   position: "Event Coordinator",
-  //   image: rahul,
-  // },
-  // {
-  //   id: 8,
-  //   name: "Raisha Pradhananga",
-  //   position: "Executive Member",
-  //   image: Raisha,
-  // },
-  // {
-  //   id: 9,
-  //   name: "Dikshita Subedi",
-  //   position: "Executive Member",
-  //   image: dikshita,
-  // },
-  // {
-  //   id: 10,
-  //   name: "Ritika Suwal",
-  //   position: "Executive Member",
-  //   image: ritika,
-  // },
-  // {
-  //   id: 11,
-  //   name: "Maharshi Kattel",
-  //   position: "Executive Member",
-  //   image: Maharshi,
-  // },
 ];
 
 export default users;
