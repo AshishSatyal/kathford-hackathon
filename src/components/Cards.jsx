a8ef99fa27bcd970c1829d5d57cb8eddf5bd6b30
AshishSatyal <ashishsatyal4@gmail.com>
2024-06-07 16:16:38 +0545

    fix:text color

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -35,9 +35,9 @@ const Cards = ({ position, prize, other, img }) => {
 
       <div className='border cursor-none z-10 border-black/[0.2] dark:border-white/[0.2] flex flex-col items-start max-w-sm mx-auto p-4 relative h-[30rem]'>
         {/* <Icon className='absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black' /> */}
-        <Icon className='absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black' />
-        <Icon className='absolute h-6 w-6 -top-3 -right-3 dark:text-white text-black' />
-        <Icon className='absolute h-6 w-6 -bottom-3 -right-3 dark:text-white text-black' />
+        <Icon className='absolute h-6 w-6 -bottom-3 -left-3 text-white' />
+        <Icon className='absolute h-6 w-6 -top-3 -right-3 text-white' />
+        <Icon className='absolute h-6 w-6 -bottom-3 -right-3 text-white' />
 
         <EvervaultCard text={position} />
         <img
